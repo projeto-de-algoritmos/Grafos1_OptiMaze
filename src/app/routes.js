c8525afda621741d3cc9d6bc5a4dc59aa56518fe
senaarth <senaarth@gmail.com>
2020-09-05 18:21:11 -0300

    Feat: Creating Home Page.

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,16 +1,7 @@
 module.exports = (app) => {
 
     app.get('/', function(req, res) {
-        res.send(`
-            <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Hello World </h1>
-            </body> 
-            </html>
-        `);
+        res.sendFile('./views/HomePage/index.html', { root: __dirname });
     })
     
 }

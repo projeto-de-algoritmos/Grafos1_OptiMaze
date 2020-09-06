0f8c46af310741a36cea9a6752a7dc748237fd81
senaarth <senaarth@gmail.com>
2020-09-06 14:36:10 -0300

    Feat: Updating routes.

--- src/app/routes.js
+++ src/app/routes.js
@@ -3,5 +3,13 @@ module.exports = (app) => {
     app.get('/', function(req, res) {
         res.render('index');
     })
-    
+
+    app.get('/play', function(req, res) {
+        res.render('play');
+    })
+
+    app.get('/instructions', function(req, res) {
+        res.render('instructions');
+    })
+
 }

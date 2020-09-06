17ab8db0181f04cd9d66014600c2056d5d433d50
senaarth <senaarth@gmail.com>
2020-09-06 12:21:15 -0300

    Feat: Setting view engine.

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const routes = require('../app/routes');
 
+app.set('view engine', 'ejs');
+app.set('views', './src/app/views')
+
 routes(app);
 
 module.exports = app;
